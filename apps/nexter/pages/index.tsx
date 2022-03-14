d0f9024077dc32c5dbc2c478f3299065d4ab7618
Volker Chartier <volker.chartier@rwe.com>
2022-03-14 11:19:14 +0100

    message

--- apps/nexter/pages/index.tsx
+++ apps/nexter/pages/index.tsx
@@ -11,7 +11,7 @@ export function Index() {
       <div className="wrapper">
         <div className="container">
           <div id="welcome">
-            <h1>asdfsdfsdfsdf</h1>
+            <h1>xdfgdfgdf</h1>
           </div>
 
           <div id="hero" className="rounded">
